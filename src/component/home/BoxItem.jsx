7d2157512b955d3938d9800832d815dbacd443df
jaydip Sutariya <jaydipsutariya123@gmail.com>
2023-12-26 16:32:47 +0530

    to fix homePage layout

--- src/component/home/BoxItem.jsx
+++ src/component/home/BoxItem.jsx
@@ -5,17 +5,15 @@ import { NavLink } from "react-router-dom";
 function BoxItem({ Imgurl, title, key }) {
   
   return (
-    <NavLink to="/shop/men">
     <div className="container" key={key}>
       <div className="box">
-        <img src="https://crwn-shop.netlify.app/images/hats.png" alt="" />
+        <img src="	https://crwn-shop.netlify.app/images/hats.png" alt="" />
       </div>
-      <div className="childbox">
+    <NavLink to="/shop/men" className="nlink">
         <h2>Hats</h2>
         <p>SHOP NOW</p>
-      </div>
-    </div>
     </NavLink>
+    </div>
   );
 }
 
--- src/component/shope/Shopepage.jsx
+++ src/component/shope/Shopepage.jsx
@@ -6,17 +6,18 @@ function Shopepage() {
   const data = useSelector((state) => {
     return state.datas;
   });
-  // console.log(Object.keys(data).map((key)=>key));
+  console.log(Object.keys(data));
   return (
     <>
-    <div className="shopepadding">
-       {Object.keys(data).reverse().map((key)=>{
-         return (
-         <Collection title={key} />)
-    })} 
-      {/* <Collection /> */}
+      <div className="shopepadding">
+        {Object.keys(data)
+          .reverse()
+          .map((key) => {
+            return <Collection title={key} />;
+          })}
+        {/* <Collection /> */}
 
-      {/* <Collection />
+        {/* <Collection />
       <Collection />
       <Collection /> */}
       </div>
